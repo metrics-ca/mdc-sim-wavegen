d877e7adc6942a5269aa6ee6afb06043474c9ccb
Shaun Luong <shaun.luong@metrics.ca>
2024-04-02 11:38:29 -0600

    Added new folder 'metrics' to consolidate all files to sim design with different Metrics tools.

---
+++ metrics/cloud_interactive.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# cloud_interactive.sh
+# Bash script to simulate design with Metrics DSim Cloud
+
+# Set up environment
+cd work
+WAVE_FILE=waves.mxd
+set -e
+
+# Compile design libraries
+mdc dvlcom -a '-lib fifo_generator_v13_2_7 +incdir+../../../../../wavegen.srcs/sources_1/imports/Sources +incdir+../../../../../wavegen.gen/sources_1/ip/clk_core ../../../../../wavegen.ip_user_files/ipstatic/simulation/fifo_generator_vlog_beh.v'
+mdc dvhcom -a '-vhdl93 -lib fifo_generator_v13_2_7  ../../../../../wavegen.ip_user_files/ipstatic/hdl/fifo_generator_v13_2_rfs.vhd'
+mdc dvlcom -a '-lib fifo_generator_v13_2_7 +incdir+../../../../../wavegen.srcs/sources_1/imports/Sources +incdir+../../../../../wavegen.gen/sources_1/ip/clk_core ../../../../../wavegen.ip_user_files/ipstatic/hdl/fifo_generator_v13_2_rfs.v'
+mdc dvlcom -a '-lib xil_defaultlib +incdir+../../../../../wavegen.srcs/sources_1/imports/Sources +incdir+../../../../../wavegen.gen/sources_1/ip/clk_core -F ../common/xil_defaultlib_filelist.txt'
+
+# Compile glbl module
+mdc dvlcom -a '-lib xil_defaultlib ../../xsim/glbl.v'
+
+# Elaborate and Simulate design, and dump waveform
+mdc dsim -a "-top xil_defaultlib.tb_wave_gen -top xil_defaultlib.glbl -run-until 1000us +acc+b -waves $WAVE_FILE -L xil_defaultlib -L xpm -L unisims_ver -L fifo_generator_v13_2_7 -lib xil_defaultlib"
---
+++ metrics/dsim_local.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# dsim_local.sh
+# Bash script to simulate design with Metrics DSim on local machine
+
+# Set up environment
+cd work
+WAVE_FILE=waves.mxd
+set -e
+
+# Compile standard libraries
+dlib map -lib ieee ${STD_LIBS}/ieee93
+dvhcom -vhdl93 -lib xpm -F ../common/xpm_filelist.txt
+dvhcom -vhdl93 -lib unisim -F ../common/unisim_filelist.txt
+dvlcom -lib xpm -F ../common/xpm_ver_filelist.txt
+dvlcom -lib unisims_ver -F ../common/unisims_ver_filelist.txt
+
+# Compile design libraries
+dvlcom -lib fifo_generator_v13_2_7 +incdir+../../../../../wavegen.srcs/sources_1/imports/Sources +incdir+../../../../../wavegen.gen/sources_1/ip/clk_core ../../../../../wavegen.ip_user_files/ipstatic/simulation/fifo_generator_vlog_beh.v
+dvhcom -vhdl93 -lib fifo_generator_v13_2_7  ../../../../../wavegen.ip_user_files/ipstatic/hdl/fifo_generator_v13_2_rfs.vhd
+dvlcom -lib fifo_generator_v13_2_7 +incdir+../../../../../wavegen.srcs/sources_1/imports/Sources +incdir+../../../../../wavegen.gen/sources_1/ip/clk_core ../../../../../wavegen.ip_user_files/ipstatic/hdl/fifo_generator_v13_2_rfs.v
+dvlcom -lib xil_defaultlib +incdir+../../../../../wavegen.srcs/sources_1/imports/Sources +incdir+../../../../../wavegen.gen/sources_1/ip/clk_core -F ../common/xil_defaultlib_filelist.txt
+
+# Compile glbl module
+dvlcom -lib xil_defaultlib ../../xsim/glbl.v
+
+# Elaborate and Simulate design, and dump waveform
+dsim -top xil_defaultlib.tb_wave_gen -top xil_defaultlib.glbl -run-until 1000us +acc+b -waves $WAVE_FILE -L xil_defaultlib -L xpm -L unisims_ver -L fifo_generator_v13_2_7 -lib xil_defaultlib
